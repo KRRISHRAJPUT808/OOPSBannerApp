8dafa5a3d1bdd15784f88df3cd11f3f0ee8a628f
KRRISHRAJPUT808 <kr3986@srmist.edu.in>
2026-02-16 18:09:53 +0530

    Update OOPSBannerApp.java

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,18 +1,9 @@
-
 public class OOPSBannerApp {
 
-    
     public static void main(String[] args) {
 
-        // Print OOPS in banner style (7 lines)
-
-        System.out.println(" *****   *****   *****   ***** ");
-        System.out.println("*     * *     * *     * *      ");
-        System.out.println("*     * *     * *     *  *    ");
-        System.out.println("*     * *     * * ****    **** ");
-        System.out.println("*     * *     * *      *     *");
-        System.out.println("*     * *     * *      *     *");
-        System.out.println(" *****   *****          ****** ");
+        
+        System.out.println("OOPS");
 
     }
 }
