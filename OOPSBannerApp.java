8b54de3e2206ab577f8f99051b25b627643b22b4
KRRISHRAJPUT808 <kr3986@srmist.edu.in>
2026-03-01 16:43:52 +0530

    Update OOPSBannerApp.java

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,6 +1,6 @@
     public class OOPSBannerApp {
 
-    // Method to generate pattern for O
+   
     public static String[] buildO() {
         return new String[] {
             " ***** ",
@@ -11,7 +11,7 @@
         };
     }
 
-    // Method to generate pattern for P
+   
     public static String[] buildP() {
         return new String[] {
             " ***** ",
@@ -22,7 +22,7 @@
         };
     }
 
-    // Method to generate pattern for S
+  
     public static String[] buildS() {
         return new String[] {
             " ***** ",
@@ -33,18 +33,18 @@
         };
     }
 
-    // Main method
+ 
     public static void main(String[] args) {
 
-        // Get character patterns using methods
+       
         String[] O = buildO();
         String[] P = buildP();
         String[] S = buildS();
 
-        // Store banner letters in array
+        
         String[][] banner = { O, O, P, S };
 
-        // Print banner line by line
+       
         for (int i = 0; i < O.length; i++) {
 
             for (int j = 0; j < banner.length; j++) {
