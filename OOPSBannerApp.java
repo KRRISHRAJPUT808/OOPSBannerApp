86a6f34bdd4eb3250ff5b512cbbe6102ab8ccd89
KRRISHRAJPUT808 <kr3986@srmist.edu.in>
2026-03-01 16:38:54 +0530

    UC6: Use static helper methods to generate banner characters

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,21 +1,57 @@
-public class OOPSBannerApp {
+    public class OOPSBannerApp {
 
+    // Method to generate pattern for O
+    public static String[] buildO() {
+        return new String[] {
+            " ***** ",
+            "*     *",
+            "*     *",
+            "*     *",
+            " ***** "
+        };
+    }
+
+    // Method to generate pattern for P
+    public static String[] buildP() {
+        return new String[] {
+            " ***** ",
+            "*     *",
+            " ***** ",
+            "*      ",
+            "*      "
+        };
+    }
+
+    // Method to generate pattern for S
+    public static String[] buildS() {
+        return new String[] {
+            " ***** ",
+            "*      ",
+            " ***** ",
+            "      *",
+            " ***** "
+        };
+    }
+
+    // Main method
     public static void main(String[] args) {
 
-        String[] banner = {
+        // Get character patterns using methods
+        String[] O = buildO();
+        String[] P = buildP();
+        String[] S = buildS();
 
-            String.join(" ", " *** ", " *** ", "**** ", " ****"),
-            String.join(" ", "*   *", "*   *", "*   *", "*    "),
-            String.join(" ", "*   *", "*   *", "*   *", "*    "),
-            String.join(" ", "*   *", "*   *", "**** ", " *** "),
-            String.join(" ", "*   *", "*   *", "*    ", "    *"),
-            String.join(" ", "*   *", "*   *", "*    ", "    *"),
-            String.join(" ", " *** ", " *** ", "*    ", "**** ")
+        // Store banner letters in array
+        String[][] banner = { O, O, P, S };
 
-        };
+        // Print banner line by line
+        for (int i = 0; i < O.length; i++) {
 
-        for (String line : banner) {
-            System.out.println(line);
+            for (int j = 0; j < banner.length; j++) {
+                System.out.print(banner[j][i] + "  ");
+            }
+
+            System.out.println();
         }
     }
 }
